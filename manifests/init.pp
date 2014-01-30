f1408e22d621980b5f8203110f5669d3ad892bfb
Atze de Vries <atze.devries@naturalis.nl>
2014-01-30 15:30:48 +0100

    fixed something

--- manifests/init.pp
+++ manifests/init.pp
@@ -117,7 +117,7 @@ class fileserver(
   include nfs::server
   nfs::server::export{$sharedir:
     clients => "${nfs_allowed_ip}(rw,sync,no_root_squash)",
-    tags    => "${hostname}_nfs_backup_share",
+    nfstag  => "${hostname}_nfs_backup_share",
     require => File['sharedir'],
   }
 }
