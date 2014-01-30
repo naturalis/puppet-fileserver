b67bf1cc7b55c463cbec85754336048ed0d26f1a
Atze de Vries <atze.devries@naturalis.nl>
2014-01-30 16:06:21 +0100

    fixed something

--- manifests/init.pp
+++ manifests/init.pp
@@ -58,66 +58,66 @@ class fileserver(
   $nfs_allowed_ip = '',
 ){
   
-#  $pvs_array = split($pvs,',')
-#  
-#  physical_volume { $pvs_array:
-#  	ensure => present,
- # } ->
-  #volume_group { $vg:
-  #	ensure           => present,
-  #	physical_volumes => $pvs_array,
-  #} ->
-  #exec { $lv:
- # 	command => "/sbin/lvcreate --name ${lv} ${vg} -l 100%FREE",
- # 	creates => "/dev/$vg/$lv",
- # } ->
- # filesystem { "/dev/${vg}/${lv}":
- # 	ensure  => present,
- # 	fs_type => $fstype,
- # } ->
+  $pvs_array = split($pvs,',')
+  
+  physical_volume { $pvs_array:
+  	ensure => present,
+  } ->
+  volume_group { $vg:
+  	ensure           => present,
+  	physical_volumes => $pvs_array,
+  } ->
+  exec { $lv:
+  	command => "/sbin/lvcreate --name ${lv} ${vg} -l 100%FREE",
+  	creates => "/dev/$vg/$lv",
+  } ->
+  filesystem { "/dev/${vg}/${lv}":
+  	ensure  => present,
+  	fs_type => $fstype,
+  } ->
 
 # Create and mount share directory
-#  file { $sharedir:
-#  	ensure => directory,
-#  	path   => $sharedir,
-#  } ->
-#  mount { 'sharemount':
-#  	ensure => 'mounted',
-#  	name   => $sharedir,
-#  	device => "/dev/${vg}/${lv}",
-#  	fstype => $fstype,
-#  	remounts => true,
-#  	options => "defaults",
-#  	atboot  => "true",
-#  }
-  # ->
-  #class {'samba::server':
- 	#workgroup => 'workgroup',
-  #  server_string => "Samba Backup Server",
-  #  interfaces => "eth0 lo",
-  #  security => 'share'
-  #} ->
-  #samba::server::share {'backup-share':
-  #	comment => 'Backup Share',
-  #	path => $sharedir,
-  #	guest_only => true,
-  #	guest_ok => true,
-  #	guest_account => "guest",
-  #	browsable => false,
-  #	create_mask => 0777,
-  #	force_create_mask => 0777,
-  #	directory_mask => 0777,
-  #	force_directory_mask => 0777,
+  file { $sharedir:
+  	ensure => directory,
+  	path   => $sharedir,
+  } ->
+  mount { 'sharemount':
+  	ensure => 'mounted',
+  	name   => $sharedir,
+  	device => "/dev/${vg}/${lv}",
+  	fstype => $fstype,
+  	remounts => true,
+  	options => "defaults",
+  	atboot  => "true",
+  } ->
+  class {'samba::server':
+ 	workgroup => 'workgroup',
+    server_string => "Samba Backup Server",
+    interfaces => "eth0 lo",
+    security => 'share'
+  } ->
+  samba::server::share {'backup-share':
+  	comment => 'Backup Share',
+  	path => $sharedir,
+  	guest_only => true,
+  	guest_ok => true,
+  	guest_account => "guest",
+  	browsable => false,
+  	create_mask => 0777,
+  	force_create_mask => 0777,
+  	directory_mask => 0777,
+  	force_directory_mask => 0777,
 #  force_group => 'group',
 #  force_user => 'user',
 #  copy => 'some-other-share',
-  #}
+  }
 
   # Nfs mount point
-  include concat::setup
-  include nfs::server
+  # include concat::setup
+  # include nfs::server
   nfs::server::export{$sharedir:
     clients => "${nfs_allowed_ip}(rw,sync,no_root_squash)",
     nfstag  => "${::hostname}_nfs_backup_share",
+    require => File[$sharedir],
   }
 }
